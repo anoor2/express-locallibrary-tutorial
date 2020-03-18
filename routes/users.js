16aed69b73ee800797c8ed292178fbea8d0df00f
Aneesa N <38112523+anoor2@users.noreply.github.com>
2020-03-18 18:25:46 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.end('There are currently no users');
+  res.send('respond with a resource');
 });
 
-/* mini challenge route
-router.get('/cool', function(req, res, next) {
-  res.send('You\'re so cool');
-});
-*/
 module.exports = router;
+~
