ab19d1b448bc1671f5a1b945c22a37edca90bf7f
Aneesa N <38112523+anoor2@users.noreply.github.com>
2020-03-18 18:25:08 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/*
-// GET home page. Initial
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'GEEKSKOOL Mini Library' });
-});
-*/
-
-
-// GET Home page
+// GET home page.
 router.get('/', function(req, res) {
-  res.redirect('/catalog')
-})
+  res.redirect('/catalog');
+});
 
 module.exports = router;
+
